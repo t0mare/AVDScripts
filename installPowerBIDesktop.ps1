341d4b715c1e925d4db4724f45c03a17619bc33c
Tom Are Tørum <tat@igelkott.net>
2025-02-19 08:56:04 +0100

    Update installPowerBIDesktop.ps1

--- installPowerBIDesktop.ps1
+++ installPowerBIDesktop.ps1
@@ -18,10 +18,12 @@ try {
     Start-Process -NoNewWindow -Wait -FilePath "winget.exe" -ArgumentList "install --id 9NTXR16HNW1T --source msstore --silent --accept-package-agreements"
 
     # Verify Installation Path
-    if (Test-Path "C:\Program Files\Microsoft Power BI Desktop\bin\PBIDesktop.exe") {
-        Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -EventId 1002 -Message "✅ Power BI installed successfully."
-    } else {
-        Write-EventLog -LogName $LogName -Source $LogSource -EntryType Warning -EventId 1003 -Message "⚠️ Power BI installation completed, but executable not found."
+    $PowerBI = Get-AppxPackage -Name "Microsoft.MicrosoftPowerBIDesktop"
+    if ($PowerBI) {
+        Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -EventId 1002 -Message "✅ Power BI installed successfully via Microsoft Store."
+    } 
+    else {
+        Write-EventLog -LogName $LogName -Source $LogSource -EntryType Warning -EventId 1003 -Message "⚠️ Power BI installation not found."
     }
 }
 catch {
